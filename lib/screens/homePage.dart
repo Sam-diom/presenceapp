c5ff2e9ba50af21dac214bae0085784f7da24c15
Soro noura <soronoor20@gmail.com>
2023-10-11 12:13:31 +0000

    connect loginPage and HomePage

--- lib/screens/homePage.dart
+++ lib/screens/homePage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
+  static const String id = 'home';
   const HomePage({super.key});
   @override
   Widget build(BuildContext context) {
