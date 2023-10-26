9e1cad707e132e9f24ca18dc0f356ef1c2b71261
Sam-diom <samueldiomande42@gmail.com>
2023-10-26 11:57:32 +0000

    rr

---
+++ lib/r.dart
@@ -0,0 +1,10 @@
+class AssetAssets {
+	static const String img1 = 'assets/img1.png';
+	static const String img1 = 'assets/img1.svg';
+	static const String img2 = 'assets/img2.png';
+	static const String img2 = 'assets/img2.svg';
+	static const String img3 = 'assets/img3.png';
+	static const String img3 = 'assets/img3.svg';
+	static const String logoInTime = 'assets/logo in time.png';
+}
+
